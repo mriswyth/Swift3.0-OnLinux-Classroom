6c0b4ab63e6a3f9ec8f1ac7b8a522d7dc091bfb6
Bryan Thompson <ghmris@mristek.com>
2016-05-05 17:43:55 -0400

    Copy Swift 2.2 playground version of build tutorial
    Add swift_org_hello for testing the build as present in the swift-2.2
    playground.
    
    These are the initial commits to test the Swift 3.0 Docker environment.

---
+++ swift_org_hello/Package.swift
@@ -0,0 +1,13 @@
+/* 
+	Swift.org Hello World Package
+	https://swift.org/getting-started/#using-the-build-system
+
+	swift-build isn't part of the Swift 2.2 Release so this seems to be 
+	a useless file right now.
+*/
+
+import PackageDescription
+
+let package = Package(
+	name: "SwiftOrgHello"
+)
---
+++ swift_org_hello/Sources/main.swift
@@ -0,0 +1,6 @@
+if (Process.arguments.count != 2) {
+	print("Usage: \(Process.arguments[0]) NAME")
+} else {
+	let name = Process.arguments[1]
+	sayHello(name)
+}
